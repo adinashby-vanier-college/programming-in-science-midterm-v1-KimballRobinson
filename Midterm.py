786965d1d49b1bc365a0593636d3b843ea22422f
KimballRobinson <kimball.b.robinson@gmail.com>
2026-01-13 10:59:40 -0500

    Update Midterm.py

--- Midterm.py
+++ Midterm.py
@@ -2,15 +2,68 @@ import math
 
 # Q1: Calculate the area of a circle
 def area_of_circle(radius):
-    return 0.0
+    """ Computes the area of circle with radius 'radius'.
+
+    Returns:
+    float: area of the circle rounded to 2 decimal points.
+    """
+    area = math.pi * radius ** 2
+    rounded_area = round(area, 2)
+
+    return rounded_area
 
 # Q2: Hollow Right Triangle
 def hollow_right_triangle(n):
-    return ""
+    """ Generates a hollow right triangle of stars of height 'n' if n > 3.
+
+    Returns:
+    string: a right triangle of height 'n' if n > 3,
+    string: a message if n <= 3
+    """
+    # checks if n is large enough
+    if n <= 3:
+        return "The triangle height should be at least 4."
+
+    triangle = ""
+    # adds the first n - 1 rows of stars and spaces to triangle
+    for i in range(n - 1):
+        triangle += "*"
+        for j in range(i - 1):
+            triangle += " "
+        if 0 < i:
+            triangle += "*"
+        triangle += "\n"
+
+    # adds the final row of stars to triangle
+    for k in range(n):
+        triangle += "*"
+
+    return triangle
 
 # Q3: Inverted Pyramid
 def inverted_pyramid(n):
-    return ""
+    """ Generates an inverted pyramid of height 'n' if n > 2.
+    
+    Returns:
+    string: an inverted pyramid of height 'n' if n > 2,
+    string: a message if n <= 2
+    """
+    # checks if n is large enough
+    if n < 3:
+        return "The pyramid height should be at least 3."
+    
+    pyramid = ""
+    for i in range(n):
+        # adds i spaces at the start of each row
+        for j in range(i):
+            pyramid += " "
+        # adds 2n-2i-1 stars to each row
+        for k in range(2 * (n - i) - 1):
+            pyramid += "*"
+        pyramid += "\n"
+
+    # returns pyramid without the last "\n"
+    return pyramid.rstrip()
 
 # ----------------------------------------------------------------
 print(area_of_circle(5))
@@ -25,10 +78,10 @@ print()
 print(hollow_right_triangle(5))
 print()
 
-print(inverted_pyramid(3))
+print(inverted_pyramid(2))
 print()
 
-print(inverted_pyramid(4))
+print(inverted_pyramid(3))
 print()
 
 print(inverted_pyramid(5))
